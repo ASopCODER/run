6919058d05d5877b9970a4b54947f80172ee9048
ASopCODER <109585475+ASopCODER@users.noreply.github.com>
2023-07-20 17:57:28 +0530

    Add files via upload

---
+++ run check/test.py
@@ -0,0 +1,5 @@
+def my_function(fname):
+    print(fname+"Refsence")
+my_function("Emil")
+my_function("tobies")
+my_function("linus")
---
+++ run check/test2.py
@@ -0,0 +1,7 @@
+a = 33   
+b = "33"
+if b > a:
+    print("b is greater than a") 
+elif a==b:
+    print("a and b are equal")
+    #this is a worng code ,it will not work because we cant compare a str and int
